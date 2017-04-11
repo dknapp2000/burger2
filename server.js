8941dee886eeafab884148de0077c457a603da86
Don Knapp <don@dknapp.com>
2017-04-10 21:11:18 -0400

    Turn off sync+force

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require( "./controllers/burgers_controller.js" )(db);
 
 const port = process.env.PORT || 3000;
 
-db.sequelize.sync( { "force": true } ).then(function() {
+db.sequelize.sync( { "force": false } ).then(function() {
     app.listen( port, () => {
         console.log( "Listening on port " + port );
     });
