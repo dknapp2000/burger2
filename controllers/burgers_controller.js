b979f61f863272523902dc2801c9ae61ff363236
Don Knapp <don@dknapp.com>
2017-04-10 20:49:49 -0400

    Sequelize is working in burger2

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,44 +3,52 @@ const orm = require( "../controllers/burger.js" );
 const path = require( "path" );
 const app = express();
 const exphbs = require("express-handlebars");
-const db = require( "../config/orm.js" );
+// const db = require( "../config/orm.js" );
 
 const publicPath = path.join( __dirname, "public" );
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Allow static files the be sent.
-app.use(express.static( publicPath ));
+function ormInit( db ) {
+	// Allow static files the be sent.
+	app.use(express.static( publicPath ));
+	
+	// The main url to display the page.
+	app.get( "/", function( req, res ) {
+	    db.burgers.findAll({})
+	    .then( function( burgers ) { 
+	        //console.log( burgers );
+	        res.render( "index", { "burgers": burgers } )
+	    }); 
+	});
+	
+	// Eat a burger . . .
+	app.post( "/api/chomp/:id", function( req, res ) {
+	    // console.log( "Chomp: ", req.url );
+	    // console.log( req.params );
+	    db.burgers.update( 
+                { "devoured": 1 }, 
+                { where: {"id": req.params.id  } } 
+        )
+	    .then( ( resp ) => {
+	        console.log( "Redirecting to /")
+	        res.redirect( "/" );
+	    })
+	});
+	
+	// Add a new burger to the table
+	app.post( "/api/add/:name", function( req, res ) {
+	    console.log( req.params );
+	    // db.insertOne( { "burger_name": req.params.name, "devoured": 0 })
+	    db.burgers.create( { "burger_name": req.params.name, "devoured": 0 })
+	    .then( (resp ) => {
+	        console.log( "redirecting to /" );
+	        res.redirect( "/" );
+	    })
+	})
 
-// The main url to display the page.
-app.get( "/", function( req, res ) {
-    db.selectAll()
-    .then( function( burgers ) { 
-        //console.log( burgers );
-        res.render( "index", { "burgers": burgers } )
-    }); 
-});
+    return app;
+}
 
-// Eat a burger . . .
-app.post( "/api/chomp/:id", function( req, res ) {
-    // console.log( "Chomp: ", req.url );
-    // console.log( req.params );
-    db.updateOne( { "devoured": 1, "id": req.params.id } )
-    .then( ( resp ) => {
-        console.log( "Redirecting to /")
-        res.redirect( "/" );
-    })
-});
-
-// Add a new burger to the table
-app.post( "/api/add/:name", function( req, res ) {
-    console.log( req.params );
-    db.insertOne( { "burger_name": req.params.name, "devoured": 0 })
-    .then( (resp ) => {
-        console.log( "redirecting to /" );
-        res.redirect( "/" );
-    })
-})
-
-module.exports = app;
+module.exports = ormInit;;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const app = require( "./controllers/burgers_controller.js" );
 
 const db = require( "./models" );
 
+const app = require( "./controllers/burgers_controller.js" )(db);
+
 const port = process.env.PORT || 3000;
 
-db.sequelize.sync( { force: true } ).then(function() {
+db.sequelize.sync( { } ).then(function() {
     app.listen( port, () => {
         console.log( "Listening on port " + port );
     });
